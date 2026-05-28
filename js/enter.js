a88d5c0d3a69ccd5b5011f960a0bd767bdad8f55
foloram <ghazali.ajdar78@gmail.com>
2026-05-28 21:12:19 +0200

    correct code

--- js/enter.js
+++ js/enter.js
@@ -15,28 +15,22 @@ window.enterHouse = function () {
 
   const spawn = document.querySelector("#spawn-point");
 
-  // 🌍 ورود به دنیای داخل
   outside.setAttribute("visible", "false");
   inside.setAttribute("visible", "true");
 
-  // 🌫 حذف fog فقط هنگام ورود
   scene.removeAttribute("fog");
 
-  // 💡 خاموش کردن نور بیرون
   outsideLights.setAttribute("visible", "false");
 
-  // 🔊 قطع صدای بیرون
   windSound.pause();
   windSound.currentTime = 0;
 
-  // 🔊 صدای داخل
   insideSound.volume = 0.6;
 
   insideSound.play().catch((err) => {
     console.log(err);
   });
 
-  // 🚶 انتقال بازیکن
   setTimeout(() => {
     const worldPos = spawn.object3D.getWorldPosition(new THREE.Vector3());
 
